43487ae5f7be817fb1c5be2752386fd708826379
romanovsqd <romanovsqd@gmail.com>
2025-11-24 18:16:08 +0300

    feat(cards): add cards urls and include to root URLconf

--- devshub/urls.py
+++ devshub/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls', namespace='users')),
+    path('cards/', include('cards.urls', namespace='cards')),
 ]
