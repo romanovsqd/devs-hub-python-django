731e0dfb02ce7d266de77e310e577a6010fd990b
romanovsqd <romanovsqd@gmail.com>
2025-11-25 16:18:09 +0300

    feat(cards): add template, url and view for card list

--- cards/urls.py
+++ cards/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'cards'
 
 urlpatterns = [
+    path('', views.card_list, name='card_list'),
     path('create/', views.card_create, name='card_create'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Card
 from .forms import CardForm
+
+
+@login_required
+def card_list(request):
+    cards = Card.objects.all()
+
+    context = {
+        'cards': cards
+    }
+    return render(request, 'cards/card_list.html', context)
 
 
 @login_required
