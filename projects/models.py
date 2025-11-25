aefdb902642f9c76722e2bdae49e39d533a9994c
romanovsqd <romanovsqd@gmail.com>
2025-11-25 22:39:23 +0300

    feat(projects): add project model and initial migration

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    repository_url = models.URLField(max_length=255, blank=True)
+    live_url = models.URLField(max_length=255, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='projects',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['title']),
+        ]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            'projects:project_detail', kwargs={'project_id': self.pk}
+        )
