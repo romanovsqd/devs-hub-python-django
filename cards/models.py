72c924304efbc6cae9c1c4360c9744563f044a15
romanovsqd <romanovsqd@gmail.com>
2025-11-25 15:41:06 +0300

    feat(cards): add card model and initial migration

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    front = models.CharField(max_length=255)
+    back = models.TextField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='cards',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['front']),
+        ]
+
+    def __str__(self):
+        return self.front
