0c8f3a7029a9e735f390bd0e2cd43da01ce7a9db
romanovsqd <romanovsqd@gmail.com>
2025-11-27 18:27:59 +0300

    feat(cardsets): add CardSetForm

--- cards/forms.py
+++ cards/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Card
+from .models import Card, CardSet
 
 
 class CardForm(forms.ModelForm):
     class Meta:
         model = Card
         fields = ['question', 'answer']
+
+
+class CardSetForm(forms.ModelForm):
+    class Meta:
+        model = CardSet
+        fields = ['title', 'cards']
