4cdd6fe812b7846bdcadc9ae56880fe84eddf37e
romanovsqd <romanovsqd@gmail.com>
2025-12-02 23:19:04 +0300

    feat(cardsets): add created_at and updated_at fields

--- cards/models.py
+++ cards/models.py
@@ -81,6 +81,8 @@ class CardSetProgress(models.Model):
         related_name='cardset_progresses',
         on_delete=models.CASCADE
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ['learner', 'cardset', 'card']
