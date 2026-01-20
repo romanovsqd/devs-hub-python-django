59fd086a2d26b1ebb61fd7e186aefae7d1a09046
romanovsqd <romanovsqd@gmail.com>
2026-01-20 14:24:51 +0300

    refactor(cards): rewrite toggle_save script to async await style

--- cards/static/cards/js/toggle_save.js
+++ cards/static/cards/js/toggle_save.js
@@ -2,39 +2,40 @@ const toggleSaveForm = document.querySelector('[data-js-toggle-save-form]');
 
 if (toggleSaveForm) {
   const toggleButton = toggleSaveForm.querySelector('[data-js-toggle-button]');
-  toggleSaveForm.addEventListener('submit', (e) => {
+
+  toggleSaveForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const csrftoken = Cookies.get('csrftoken');
+    try {
+      const csrftoken = Cookies.get('csrftoken');
 
-    fetch(toggleSaveForm.action, {
-      method: toggleSaveForm.method,
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': csrftoken,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Ошибка сервера: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        toggleButton.textContent = data.button_text;
-        showNotification(data.message);
-      })
-      .catch((error) => {
-        console.error(error);
-
-        showNotification('Произошла ошибка. Попробуйте снова.')
+      const response = await fetch(toggleSaveForm.action, {
+        method: toggleSaveForm.method,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrftoken,
+        },
       });
+
+      if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      toggleButton.textContent = data.button_text;
+      showNotification(data.message);
+    } catch (error) {
+      console.error(error);
+      showNotification('Произошла ошибка. Попробуйте снова.');
+    }
   });
 }
 
 function showNotification(message) {
   const notification = document.createElement('div');
   notification.textContent = message;
+
   document.body.appendChild(notification);
   setTimeout(() => notification.remove(), 3000);
 }
