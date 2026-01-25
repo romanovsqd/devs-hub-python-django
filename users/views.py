c38a055599c26c578af2a724bb9a078794c4ba29
romanovsqd <romanovsqd@gmail.com>
2026-01-25 16:10:41 +0300

    refactor(users): change user variable name int users views and templates

--- users/views.py
+++ users/views.py
@@ -57,54 +57,55 @@ def user_list(request):
 
 @login_required
 def user_detail(request, user_id):
-    current_user = get_object_or_404(User, pk=user_id)
+    user = get_object_or_404(User, pk=user_id)
 
     context = {
-        'current_user': current_user,
+        'user': user,
     }
     return render(request, 'users/users/user_detail.html', context)
 
 
 @login_required
 def user_cards(request, user_id):
-    current_user = get_object_or_404(User, pk=user_id)
-    cards = Card.objects.filter(
-        Q(author=current_user) | Q(saved_by=current_user)
+    user = get_object_or_404(User, pk=user_id)
+    user_cards = Card.objects.filter(
+        Q(author=user) | Q(saved_by=user)
     ).distinct()
 
     context = {
-        'current_user': current_user,
-        'cards': cards,
+        'user': user,
+        'user_cards': user_cards,
     }
     return render(request, 'users/users/user_cards.html', context)
 
 
 @login_required
 def user_cardsets(request, user_id):
-    current_user = get_object_or_404(User, pk=user_id)
-    cardsets = CardSet.objects.filter(
-        Q(author=current_user) | Q(saved_by=current_user)
+    user = get_object_or_404(User, pk=user_id)
+    user_cardsets = CardSet.objects.filter(
+        Q(author=user) | Q(saved_by=user)
     ).distinct()
 
-    studying_cardsets_ids = CardSetProgress.objects.filter(
-        learner=current_user,
-    ).values_list('cardset_id', flat=True)
+    # TODO: Перенести в profile_deatil view
+    # studying_cardsets_ids = CardSetProgress.objects.filter(
+    #     learner=user,
+    # ).values_list('cardset_id', flat=True)
 
     context = {
-        'current_user': current_user,
-        'cardsets': cardsets,
-        'studying_cardsets_ids': studying_cardsets_ids,
+        'user': user,
+        'user_cardsets': user_cardsets,
+        # 'studying_cardsets_ids': studying_cardsets_ids,
     }
     return render(request, 'users/users/user_cardsets.html', context)
 
 
 @login_required
 def user_projects(request, user_id):
-    current_user = get_object_or_404(User, pk=user_id)
-    projects = current_user.projects.all()
+    user = get_object_or_404(User, pk=user_id)
+    user_projects = user.projects.all()
 
     context = {
-        'current_user': current_user,
-        'projects': projects,
+        'user': user,
+        'user_projects': user_projects,
     }
     return render(request, 'users/users/user_projects.html', context)
