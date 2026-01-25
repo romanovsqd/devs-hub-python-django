ec2919c595ba8431d54e8996cea95079af7d8b42
romanovsqd <romanovsqd@gmail.com>
2026-01-25 10:50:03 +0300

    feat(users): extend User model with profile fields
    - add specialization field
    - add primary_skill field
    - add avatar field
    - add __str__ method
    - make migration

--- users/models.py
+++ users/models.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.urls import reverse
 
 
 class User(AbstractUser):
+    specialization = models.CharField(max_length=50, blank=True)
+    primary_skill = models.CharField(max_length=50, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('users:user_detail', kwargs={'user_id': self.pk})
+
+    def __str__(self):
+        return f'{self.username} ({self.specialization})'
