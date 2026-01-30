e695c1f34d3ae95202320ec6c8ae6441fcb65ac0
romanovsqd <romanovsqd@gmail.com>
2026-01-30 14:23:41 +0300

    refactor(carsets): change card-sets route to cardsets in root URLconf

--- devshub/urls.py
+++ devshub/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls', namespace='users')),
     path('cards/', include('cards.urls', namespace='cards')),
-    path('card-sets/', include('cards.urls_cardsets', namespace='cardsets')),
+    path('cardsets/', include('cards.urls_cardsets', namespace='cardsets')),
     path('study/', include('cards.study_urls', namespace='study')),
     path('projects/', include('projects.urls', namespace='projects')),
 ] + debug_toolbar_urls()
