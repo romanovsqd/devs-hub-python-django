2d771256c9fa86ceb5b4a60ddf0890155567a3c7
romanovsqd <romanovsqd@gmail.com>
2026-02-13 01:34:59 +0300

    refactor(templatetags): reorganize structure of components

--- devshub_project/cards/templatetags/card_tags.py
+++ devshub_project/cards/templatetags/card_tags.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("cards/components/card.html")
+@register.inclusion_tag("components/card.html")
 def render_card(card):
     return {"card": card}
--- devshub_project/decks/templatetags/deck_tags.py
+++ devshub_project/decks/templatetags/deck_tags.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("decks/components/deck.html")
+@register.inclusion_tag("components/deck.html")
 def render_deck(deck):
     return {"deck": deck}
