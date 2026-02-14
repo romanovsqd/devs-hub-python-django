9c35fd8b6ba2c94500cda9a71b7837d9c1bc1fd0
romanovsqd <romanovsqd@gmail.com>
2026-02-14 20:11:34 +0300

    style(decks): add markup and styles for decks_list template
    - apply css styles for deck_list.html
    - create __init__.py in templatetags direcory
    - add markup, styles and logic to render_deck tag

--- devshub_project/decks/templatetags/deck_tags.py
+++ devshub_project/decks/templatetags/deck_tags.py
@@ -1,8 +1,18 @@
 from django import template
 
+from decks import services
+
 register = template.Library()
 
 
 @register.inclusion_tag("components/deck.html")
-def render_deck(deck):
-    return {"deck": deck}
+def render_deck(deck, user, studying_decks_ids=None, is_owner=None):
+    is_author = deck.author == user
+    is_saved = services.is_deck_saved_by_user(deck, user)
+    return {
+        "deck": deck,
+        "is_author": is_author,
+        "is_saved": is_saved,
+        "studying_decks_ids": studying_decks_ids,
+        "is_owner": is_owner,
+    }
