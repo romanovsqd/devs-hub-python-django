4e055f5778f53019e29f15ecd2e2d09bc8cb39cb
romanovsqd <romanovsqd@gmail.com>
2026-02-14 20:08:15 +0300

    style(cards): add markup and styles for card_list template
    - apply css styles for card_list.html
    - add markup, styles and logic to render_card tag

--- devshub_project/cards/templatetags/card_tags.py
+++ devshub_project/cards/templatetags/card_tags.py
@@ -1,8 +1,16 @@
 from django import template
 
+from cards import services
+
 register = template.Library()
 
 
 @register.inclusion_tag("components/card.html")
-def render_card(card):
-    return {"card": card}
+def render_card(card, user):
+    is_author = card.author == user
+    is_saved = services.is_card_saved_by_user(card=card, user=user)
+    return {
+        "card": card,
+        "is_author": is_author,
+        "is_saved": is_saved,
+    }
