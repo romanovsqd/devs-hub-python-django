0bfdcc3b410eccd64c458261c2b1af2eb8df8ae5
romanovsqd <romanovsqd@gmail.com>
2026-02-20 10:16:01 +0300

    fix(header): fix open burger menu if user is not authenticated

--- devshub_project/static/js/header.js
+++ devshub_project/static/js/header.js
@@ -4,14 +4,16 @@ const userMenu = document.querySelector("[data-js-user-menu]");
 const burgerButton = document.querySelector("[data-js-burger-button]");
 const mobileMenu = document.querySelector("[data-js-mobile-menu]");
 
-userMenuButton.addEventListener("click", (e) => {
-  e.stopPropagation();
-  userMenu.classList.toggle("hidden");
-});
+if (userMenuButton && userMenu) {
+  userMenuButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    userMenu.classList.toggle("hidden");
+  });
 
-userMenu.addEventListener("click", () => {
-  userMenu.classList.add("hidden");
-});
+  userMenu.addEventListener("click", () => {
+    userMenu.classList.add("hidden");
+  });
+}
 
 burgerButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
