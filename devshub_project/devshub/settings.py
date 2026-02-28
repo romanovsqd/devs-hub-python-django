227ffe5b5c1f1d296a4785655e107d6a986c2899
romanovsqd <romanovsqd@gmail.com>
2026-02-28 12:28:00 +0300

    feat(api): add djangorestframework, update requirements

--- devshub_project/devshub/settings.py
+++ devshub_project/devshub/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "rest_framework",
     "easy_thumbnails",
     "tinymce",
     "core.apps.CoreConfig",
