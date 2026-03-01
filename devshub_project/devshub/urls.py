b4442997531e206ddfd68578b81b6bd5c69f7225
romanovsqd <romanovsqd@gmail.com>
2026-03-01 17:36:39 +0300

    feat(jwt): install simplejwt package, update settings, urls and requirements.txt

--- devshub_project/devshub/urls.py
+++ devshub_project/devshub/urls.py
@@ -3,9 +3,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("tinymce/", include("tinymce.urls")),
     path("", include("core.urls")),
     path("", include("users.auth_urls")),
