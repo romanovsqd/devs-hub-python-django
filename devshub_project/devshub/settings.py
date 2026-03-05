2c82795393eddd7b8cbd7097f4644a2311cb176e
romanovsqd <romanovsqd@gmail.com>
2026-03-05 17:39:36 +0300

    feat(api): add Session Authentication

--- devshub_project/devshub/settings.py
+++ devshub_project/devshub/settings.py
@@ -189,6 +189,7 @@ TINYMCE_DEFAULT_CONFIG = {
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
