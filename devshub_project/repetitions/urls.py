e6c164f62bdc715ab71611dc95c000e480ab2a45
romanovsqd <romanovsqd@gmail.com>
2026-03-21 09:05:03 +0300

    refactor(repetitions): remove next_card and submit views and urls

--- devshub_project/repetitions/urls.py
+++ devshub_project/repetitions/urls.py
@@ -3,10 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        "<int:deck_id>/toggle-study/", views.deck_toggle_study, name="deck_toggle_study"
-    ),
     path("review/", views.review, name="review"),
-    path("review/next-card/", views.next_card, name="next_card"),
-    path("review/<int:deck_id>/<int:card_id>/submit/", views.submit, name="submit"),
 ]
--- devshub_project/repetitions/views.py
+++ devshub_project/repetitions/views.py
@@ -1,63 +1,7 @@
-import json
-
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
 from django.shortcuts import render
-from django.views.decorators.http import require_POST
-
-from decks import services as deck_services
-
-from . import services
-
-
-@login_required
-@require_POST
-def deck_toggle_study(request, deck_id):
-    deck = deck_services.get_deck_created_or_saved_by_user(
-        deck_id=deck_id, user=request.user
-    )
-
-    success, message = services.toggle_deck_study_by_user(deck=deck, user=request.user)
-
-    return JsonResponse({"success": success, "message": message})
 
 
 @login_required
 def review(request):
     return render(request, "repetitions/review.html")
-
-
-@login_required
-def next_card(request):
-    card_data = services.get_next_card_for_review(user=request.user)
-
-    if card_data:
-        return JsonResponse(card_data)
-    else:
-        return JsonResponse({"done": True})
-
-
-@login_required
-@require_POST
-def submit(request, deck_id, card_id):
-    data = json.loads(request.body)
-    quality = int(data.get("quality", 0))
-
-    card_progress = services.get_deck_card_progress_for_user(
-        deck_id=deck_id,
-        card_id=card_id,
-        user=request.user,
-    )
-
-    progress = services.apply_sm2(progress=card_progress, quality=quality)
-
-    return JsonResponse(
-        {
-            "deck_id": deck_id,
-            "card_id": card_id,
-            "next_review_date": progress.next_review_date.isoformat(),
-            "interval": progress.interval,
-            "efactor": progress.efactor,
-            "repetitions": progress.repetitions,
-        }
-    )
