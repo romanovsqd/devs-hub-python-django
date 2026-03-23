7fed167cc00d93c4efe326288bed6a34b0376398
romanovsqd <romanovsqd@gmail.com>
2026-03-23 11:05:04 +0300

    tests(projects): add unit tests for projects
    - Added test_create_project
    - Added test_create_project_without_author

--- devshub_project/projects/tests.py
+++ devshub_project/projects/tests.py
@@ -1,3 +1,35 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from users import services as user_services
+
+from . import services
+
+
+class ProjectTest(TestCase):
+    def setUp(self):
+        self.user = user_services.create_user("user", "password123")
+
+    def test_create_project(self):
+        project = services.create_project(
+            author=self.user,
+            title="test project title",
+            description="test project description",
+        )
+
+        self.assertEqual(project.author, self.user)
+        self.assertEqual(project.title, "test project title")
+        self.assertEqual(project.description, "test project description")
+
+        self.assertEqual(services.get_projects().count(), 1)
+
+        db_project = services.get_projects().first()
+        self.assertEqual(project.id, db_project.id)
+
+    def test_create_project_without_author(self):
+        with self.assertRaises((IntegrityError)):
+            services.create_project(
+                author=None,
+                title="test project title",
+                description="test project description",
+            )
