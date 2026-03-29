39cf1c927bd9e9269e0f29b2d9748b1007c29618
romanovsqd <romanovsqd@gmail.com>
2026-03-29 16:00:15 +0300

    chore(settings): set HOST, PORT, USE_TLS email settings as env variables

--- devshub_project/devshub/settings.py
+++ devshub_project/devshub/settings.py
@@ -175,12 +175,12 @@ INTERNAL_IPS = [
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Email server configuration
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS")
 
 # Celery
 CELERY_BROKER_URL = config("CELERY_BROKER_URL")
